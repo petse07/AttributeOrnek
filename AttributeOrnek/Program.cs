240d8a86a6d51c3c2a3f67a7f219d806f6264200
petse07 <gozcucf@gmail.com>
2022-08-11 14:39:18 +0300

    Zorunluluk kontrolu ile Ogrenci class'ının field'leri dolmuş mu diye kontrol edilir

--- AttributeOrnek/Program.cs
+++ AttributeOrnek/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AttributeOrnek;
+
 Console.WriteLine("Hello, World!");
+
+Ogrenci ogrenci = new Ogrenci();
+ogrenci.Adi = "can";
+ogrenci.Bolum = "EE";
+if(!ZorunlulukKontrolu.Dogrula(ogrenci))
+{
+    Console.WriteLine("Öğrenci bilgileri doğrulamadan geçemedi");
+}
+else
+{
+    Console.WriteLine("Başarılı Form");
+}
